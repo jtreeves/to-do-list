d24245635bbedeb993e8d3fa8ac5280851a37da0
jtreeves <jackson.t.reeves@gmail.com>
2020-12-08 16:51:46 -0500

    [App] Delete spaces around consts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import './App.css'
 import ListItem from './components/ListItem'
 
 function App() {
-    
     const doThis1 = 'Learn React'
     const doThis2 = '???'
     const doThis3 = 'Profit'
-
     const list = [doThis1, doThis2, doThis3]
-
     const listItems = list.map((eachItem, index) => {
         return <ListItem doThis={eachItem} key={`list-item-$${index}`} />
     })
