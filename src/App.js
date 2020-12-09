90dcddc177191295da9b06c3e67988c7a963930d
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 15:39:35 -0500

    [App] Add item to list

--- src/App.js
+++ src/App.js
@@ -7,22 +7,31 @@ function App() {
     const secondItem = '???'
     const thirdItem = 'Profit'
     const [list, setList] = useState([firstItem, secondItem, thirdItem])
+    const [newItem, addItem] = useState()
+    
+    const deleteFirst = () => {
+        const shorterList = list.splice(1,(list.length - 1))
+        setList(shorterList)
+    }
+
+    const addLast = () => {
+        const longerList = [...list, newItem]
+        setList(longerList)
+    }
+    
     const listItems = list.map((eachItem, index) => {
         return <ListItem doThis={eachItem} key={`item-${index}`} />
     })
 
-    const eliminateFirst = () => {
-        const deleteFirst = list.splice(1,(list.length - 1))
-        setList(deleteFirst)
-    }
-    
     return (
         <div>
             <h1>My List</h1>
             <ul>
                 {listItems}
             </ul>
-            <button onClick={eliminateFirst}>Eliminate First Item</button>
+            <input type='text' name='newItem' onChange={(event) => addItem(event.target.value)} />
+            <button onClick={addLast}>Add Item</button>
+            <button onClick={deleteFirst}>Delete Item</button>
         </div>
     )
 }
